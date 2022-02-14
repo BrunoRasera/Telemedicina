8da96ff14faf823895e670f5a4f668f2ff3341b0
Leonardo Silva <leonardosilvap00@gmail.com>
2022-02-14 14:30:43 -0300

    pequenas alteracoes

--- consulta/views.py
+++ consulta/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
-from consulta.models import Consulta
+from consulta.models import Consulta, Paciente
 from .models import Medicos
 from .forms import ConsultaForm
 
@@ -18,7 +18,7 @@ def agendaconsulta(request):
     if request.method == 'POST':
         data = request.POST['data']
         print(data)
-        user = get_object_or_404(User, pk=request.user.id)
+        user = get_object_or_404(Paciente, pk=request.user.id)
         print(user)
         consulta = Consulta.objects.create(paciente=user, data=data)
         consulta.save()
